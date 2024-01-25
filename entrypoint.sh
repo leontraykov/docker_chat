9b9e67da1a6d343f0f457f7b04dcbd5ca397df72
Leonid <leon.traykov@gmail.com>
2024-01-25 03:14:59 -0400

    entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+check_and_create_db() {
+  DB_EXIST=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h $DB_HOST -U $POSTGRES_USER -lqt | cut -d \| -f 1 | grep -w $DB_NAME | wc -l)
+  echo "Checking if DB exists..."
+  if [ "$DB_EXIST" -eq 0 ]; then
+    echo "Database does not exist. Creating..."
+    bundle exec rake db:create
+    bundle exec rake db:migrate
+    bundle exec rake db:seed
+    echo "Database created."
+  else
+    echo "Database already exists."
+  fi
+}
+
 if [ "$1" == "tests" ]; then
   echo "Waiting for PostgreSQL to become available..."
   until pg_isready -h db -p 5432 -U user; do
@@ -32,28 +46,12 @@ if [ ! -f /opt/app-initialized/initialized ]; then
   while ! pg_isready -h db -p 5432 -U user; do
     sleep 2
   done
+  
+  check_and_create_db
 
   bundle exec bun install
   bundle exec bun run build
   bundle exec bun run build:css
-  
-  touch /opt/app-initialized/initialized
-
-  echo "Dropping the database..."
-  bundle exec rake db:drop 
-  echo "Database dropping completed."
-
-  echo "Creating the database..."
-  bundle exec rake db:create
-  echo "Database creating completed."
-
-  echo "Migrating the database..."
-  bundle exec rake db:migrate
-  echo "Database migrating completed."
-
-  echo "Seeding the database..."
-  bundle exec rake db:seed
-  echo "Database seeding completed."
 fi
 
 exec "$@"
