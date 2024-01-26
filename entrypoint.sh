d2bd3828bdda3354da90941587db90d1a109473b
Leonid <leon.traykov@gmail.com>
2024-01-26 02:27:13 -0400

    correct compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 set -e
 
+# Функция для проверки и создания базы данных
 check_and_create_db() {
   DB_HOST=db
   POSTGRES_USER=user
   POSTGRES_PASSWORD=password
-  DB_NAME=docker_chat_development
+  DB_NAME=$1  # Имя базы данных передаётся как аргумент функции
 
+  # Проверка существования базы данных
   DB_EXIST=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h $DB_HOST -U $POSTGRES_USER -lqt | cut -d \| -f 1 | grep -w $DB_NAME | wc -l)
   
-  echo "Checking if DB exists..."
+  echo "Checking if $DB_NAME exists..."
   if [ "$DB_EXIST" -eq 0 ]; then
-    echo "Database does not exist. Creating..."
-    bundle exec rake db:create
-    bundle exec rake db:migrate
-    bundle exec rake db:seed
-    echo "Database created."
+    echo "Database $DB_NAME does not exist. Creating..."
+    RAILS_ENV=$2 bundle exec rake db:create db:migrate
+    if [ ! -f /opt/app-initialized/$DB_NAME ]; then
+      bundle exec rake db:seed
+      touch /opt/app-initialized/$DB_NAME
+    fi
+    echo "Database $DB_NAME created."
   else
-    echo "Database already exists."
-    bundle exec rake db:migrate
-    bundle exec rake db:seed
+    echo "Database $DB_NAME already exists."
+    RAILS_ENV=$2 bundle exec rake db:migrate
   fi
 }
 
@@ -28,38 +31,25 @@ if [ "$1" == "tests" ]; then
   until pg_isready -h db -p 5432 -U user; do
     sleep 2
   done
+  check_and_create_db "docker_chat_test" "test"
+
   echo "PostgreSQL is available now."
-
-  bundle exec bun install
-  bundle exec bun run build
-  bundle exec bun run build:css
-
-  bundle exec rake db:create RAILS_ENV=test
-  bundle exec rake db:migrate RAILS_ENV=test
-
   echo "Running Cucumber tests..."
   bundle exec cucumber
-
   echo "Running RSpec tests..."
   bundle exec rspec -f d
-
   exit 0
+else
+  # Проверка и создание базы данных для разработки
+  check_and_create_db "docker_chat_development" "development"
+  
+  if [ ! -f /opt/app-initialized/initialized ]; then
+    touch /opt/app-initialized/initialized
+  fi
 fi
 
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
-fi
-
-if [ ! -f /opt/app-initialized/initialized ]; then
-  while ! pg_isready -h db -p 5432 -U user; do
-    sleep 2
-  done
-  
-  check_and_create_db
-
-  bundle exec bun install
-  bundle exec bun run build
-  bundle exec bun run build:css
 fi
 
 exec "$@"
