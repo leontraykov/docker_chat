71bc93f40648e713e50f03cf6205f5d61613e9df
Leonid <leon.traykov@gmail.com>
2024-01-29 00:58:28 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,32 @@ user_data = [
   { name: 'user_3', email: 'user_3@ex.co', password: 'user_3_password', password_confirmation: 'user_3_password' }
 ]
 
-users = user_data.map do |attributes|
-  User.find_or_create_by!(email: attributes[:email]) do |user|
-    user.name = attributes[:name]
-    user.password = attributes[:password]
-    user.password_confirmation = attributes[:password_confirmation]
+unless User.exists?(email: user_data.first[:email])
+  users = user_data.map do |attributes|
+    User.find_or_create_by!(email: attributes[:email]) do |user|
+      user.name = attributes[:name]
+      user.password = attributes[:password]
+      user.password_confirmation = attributes[:password_confirmation]
+    end
   end
-end
 
-chat_rooms = ChatRoom.create!([
-                                { name: 'user_1 + user_2 Chatroom' },
-                                { name: 'user_2 + user_3 Chatroom' },
-                                { name: 'Common Chatroom' }
-                              ])
+  chat_rooms = ChatRoom.create!([
+                                  { name: 'user_1 + user_2 Chatroom' },
+                                  { name: 'user_2 + user_3 Chatroom' },
+                                  { name: 'Common Chatroom' }
+                                ])
 
-chat_room_users_data = [
-  { chat_room: chat_rooms[0], user: users[0] },
-  { chat_room: chat_rooms[0], user: users[1] },
-  { chat_room: chat_rooms[1], user: users[1] },
-  { chat_room: chat_rooms[1], user: users[2] },
-  { chat_room: chat_rooms[2], user: users[0] },
-  { chat_room: chat_rooms[2], user: users[1] },
-  { chat_room: chat_rooms[2], user: users[2] }
-]
+  chat_room_users_data = [
+    { chat_room: chat_rooms[0], user: users[0] },
+    { chat_room: chat_rooms[0], user: users[1] },
+    { chat_room: chat_rooms[1], user: users[1] },
+    { chat_room: chat_rooms[1], user: users[2] },
+    { chat_room: chat_rooms[2], user: users[0] },
+    { chat_room: chat_rooms[2], user: users[1] },
+    { chat_room: chat_rooms[2], user: users[2] }
+  ]
 
-chat_room_users_data.each do |chat_room_user_attributes|
-  ChatRoomUser.find_or_create_by!(chat_room_user_attributes)
+  chat_room_users_data.each do |chat_room_user_attributes|
+    ChatRoomUser.find_or_create_by!(chat_room_user_attributes)
+  end
 end
